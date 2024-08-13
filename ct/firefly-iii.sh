ef2b2a3aedc8254226a20d32af04cb7b807e08f0
AaronKnepp <akeinstein@gmail.com>
2024-08-12 21:42:20 -0700

    Create firefly-iii.sh

---
+++ ct/firefly-iii.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+# Copyright (c) 2021-2024 
+# Author: 
+# License: MIT
+# https://github.com/tteck/Proxmox/raw/main/LICENSE
+
+function header_info {
+clear
+cat <<"EOF"
+    _______           ______     
+   / ____(_)_______  / __/ /_  __
+  / /_  / / ___/ _ \/ /_/ / / / /
+ / __/ / / /  /  __/ __/ / /_/ / 
+/_/   /_/_/   \___/_/ /_/\__, /  
+                        /____/   
+
+EOF
+}
+header_info
+echo -e "Loading..."
+APP="firefly-iii"
+var_disk="2"
+var_cpu="1"
+var_ram="512"
+var_os="debian"
+var_version="12"
+variables
+color
+catch_errors
+
+function default_settings() {
+  CT_TYPE="1"
+  PW=""
+  CT_ID=$NEXTID
+  HN=$NSAPP
+  DISK_SIZE="$var_disk"
+  CORE_COUNT="$var_cpu"
+  RAM_SIZE="$var_ram"
+  BRG="vmbr0"
+  NET="dhcp"
+  GATE=""
+  APT_CACHER=""
+  APT_CACHER_IP=""
+  DISABLEIP6="no"
+  MTU=""
+  SD=""
+  NS=""
+  MAC=""
+  VLAN=""
+  SSH="no"
+  VERB="no"
+  echo_default
+}
+
+function update_script() {
+header_info
+#TODO:HOW TO DETERMINE IF INSTALLEDif [[ ! -f /etc/apache2/sites-available/ff3.conf ]]; then msg_error "No ${APP} Installation Found!"; exit; fi
+php_version=$(php -v | head -n 1 | awk '{print $2}')
+if [[ ! $php_version == "8.3"* ]]; then
+  msg_info "Updating PHP"
+  curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
+  echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ bookworm main" >/etc/apt/sources.list.d/php.list
+#bcmath    PHP BCMath Arbitrary Precision Mathematics
+#intl    PHP Internationalization extension
+#curl    PHP Curl
+#zip    PHP Zip
+#TODO: PHP Sodium
+#gd    PHP GD
+#xml    PHP XML
+#mbstring    PHP MBString
+#sqlite3    PHP support for whatever database you're going to use.
+  apt-get update
+  apt-get install -y php8.3 php8.3-cli php8.3-{bcmath,intl,curl,zip,gd,xml,mbstring,sqlite3}
+  systemctl reload apache2
+  apt autoremove
+  msg_ok "Updated PHP"
+fi
+msg_info "Updating ${APP}"
+#TODO:CHECK bash /var/www/html/update.sh
+msg_ok "Updated Successfully"
+exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed Successfully!\n"
+echo -e "${APP} should be reachable by going to the following URL.
+         ${BL}http://${IP}${CL} \n"
